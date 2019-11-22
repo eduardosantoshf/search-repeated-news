7e9dea6ccc947cc2696a2196dd05a6f0d8808d9f
Eduardo Santos <eduardosantoshf@gmail.com>
2019-11-22 15:14:08 +0000

    tests

---
+++ src/tests/InicialTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class InicialTest {
+
+	public static void main(String[] args) {
+		
+		File f = new File("newsCategories.json")
+
+	}
+
+}
