5832ad9e2173176ab407b592bb1e463a3fdb236b
Eduardo Santos <45774025+eduardosantoshf@users.noreply.github.com>
2019-12-02 19:32:47 +0000

    ReadCSV final

--- src/modules/ReadCSV.java
+++ src/modules/ReadCSV.java
@@ -5,8 +5,8 @@ import java.io.*;
 
 public class ReadCSV {
 	
-	static ArrayList<String[]> separateNew = new ArrayList<String[]>();
-
+	static HashMap<String, String> news = new HashMap<>(); //creating the HashMap
+	
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
 		
@@ -21,15 +21,23 @@ public class ReadCSV {
 			bfr.readLine(); //ignore first line
 			
 			String line = bfr.readLine(); //read line
-			String[] line2 = {};
 			
-			while (line != null) {
-				//System.out.println(line);
-				separateNew.add(line.split("</strong"));
+			while (line != null) {//read every line
+				String t = new StringBuilder(line).reverse().toString(); //reverse line
+				String title = t.split("/", 2)[0]; //get the title of each line/news
+				String x = title.split(",", 2)[1]; //remove the category
+				String newsBody = t.split("/", 2)[1].substring(7); //get the news content from each news
 				
-				line = bfr.readLine();
- 				
+				news.put(new StringBuilder(x).reverse().toString().substring(5), 
+						new StringBuilder(newsBody).reverse().toString()); //add to the HashMap each pair title - news content
+				
+				line = bfr.readLine(); //read next line
 			}
+			
+			for (String valor: news.keySet()) {
+				System.out.printf("titulo: %s; content: %s\n", valor, news.get(valor)); //print every news (title - news content)
+			}
+			
 			
 		} catch (IOException e) {
 			
@@ -37,12 +45,6 @@ public class ReadCSV {
 			
 		}
 		
-		for (String[] news: separateNew) {
-			newsNumber++;
-			System.out.printf("news #: %d; title: %s\n", newsNumber, news[0]);
-		}
-		
 	}
-	
 
 }
