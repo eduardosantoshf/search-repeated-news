e22e73194a3aafb186472f40d5dcb4c33ffb8994
Eduardo Santos <45774025+eduardosantoshf@users.noreply.github.com>
2019-12-06 16:18:26 +0000

    News

---
+++ src/modules/News.java
@@ -0,0 +1,34 @@
+package modules;
+
+public class News {
+	private String newsTitle;
+	private String newsContent;
+	
+	//constructor
+	public News(String newsTitle, String newsContent) {
+		this.newsTitle = newsTitle;
+		this.newsContent = newsContent;
+	}
+	
+	//getters and setters
+	public String getNewsTitle() {
+		return newsTitle;
+	}
+	public void setNewsTitle(String newsTitle) {
+		this.newsTitle = newsTitle;
+	}
+	public String getNewsContent() {
+		return newsContent;
+	}
+	public void setNewsContent(String newsContent) {
+		this.newsContent = newsContent;
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return "News [newsTitle=" + newsTitle + ", newsContent=" + newsContent + "]";
+	}
+	
+	
+}
